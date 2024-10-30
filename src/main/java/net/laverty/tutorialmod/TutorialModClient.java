9342e13c9cddb056e4b9bb0c9efc0853518235b8
Kyle Laverty <k.laverty@gmail.com>
2024-10-29 21:30:42 -0400

    Initial commit

---
+++ src/main/java/net/laverty/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.laverty.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
